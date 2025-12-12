9c377ed320e3eb3efd3df17a9fc13eab44ca36f6
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-12-12 15:27:37 +0500

    task7

--- Tyuiu.PlatonovMV.Sprint6.Task7.V17.Lib/DataService.cs
+++ Tyuiu.PlatonovMV.Sprint6.Task7.V17.Lib/DataService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using tyuiu.cources.programming.interfaces.Sprint6;
 
 namespace Tyuiu.PlatonovMV.Sprint6.Task7.V17.Lib
@@ -9,35 +8,32 @@ namespace Tyuiu.PlatonovMV.Sprint6.Task7.V17.Lib
     {
         public int[,] GetMatrix(string path)
         {
-            if (string.IsNullOrWhiteSpace(path))
-                throw new ArgumentException("Путь к файлу пустой.", nameof(path));
-            if (!File.Exists(path))
-                throw new FileNotFoundException("Файл не найден.", path);
-
-            string[] lines = File.ReadAllLines(path)
-                                 .Where(x => !string.IsNullOrWhiteSpace(x))
-                                 .ToArray();
+            string[] lines = File.ReadAllLines(path);
 
             int rows = lines.Length;
             int cols = lines[0].Split(';').Length;
 
-            int[,] m = new int[rows, cols];
+            int[,] matrix = new int[rows, cols];
 
             for (int r = 0; r < rows; r++)
             {
                 string[] parts = lines[r].Split(';');
                 for (int c = 0; c < cols; c++)
-                    m[r, c] = int.Parse(parts[c]);
+                {
+                    matrix[r, c] = Convert.ToInt32(parts[c]);
+                }
             }
 
             
-            if (rows > 1)
+            for (int c = 0; c < cols; c++)
             {
-                for (int c = 0; c < cols; c++)
-                    if (m[1, c] % 2 == 0) m[1, c] = 4;
+                if (matrix[1, c] % 2 == 0)
+                {
+                    matrix[1, c] = 4;
+                }
             }
 
-            return m;
+            return matrix;
         }
     }
 }
--- Tyuiu.PlatonovMV.Sprint6.Task7.V17.Test/DataServiceTest.cs
+++ Tyuiu.PlatonovMV.Sprint6.Task7.V17.Test/DataServiceTest.cs
@@ -8,32 +8,26 @@ namespace Tyuiu.PlatonovMV.Sprint6.Task7.V17.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void GetMatrix_ReadCsv_AndChangeSecondRowEvensTo4()
+        public void GetMatrix_ReadCsv_AndChangeSecondRow()
         {
-            
             string path = Path.GetTempFileName();
-            File.WriteAllLines(path, new[]
+            File.WriteAllLines(path, new string[]
             {
                 "1;2;3",
                 "4;5;6"
             });
 
             DataService ds = new DataService();
-
-            
             int[,] m = ds.GetMatrix(path);
 
-            
             Assert.AreEqual(1, m[0, 0]);
             Assert.AreEqual(2, m[0, 1]);
             Assert.AreEqual(3, m[0, 2]);
 
-            
             Assert.AreEqual(4, m[1, 0]); // 4 -> 4
             Assert.AreEqual(5, m[1, 1]); // 5 -> 5
             Assert.AreEqual(4, m[1, 2]); // 6 -> 4
 
-            
             File.Delete(path);
         }
     }
